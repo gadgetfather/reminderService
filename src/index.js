9b7a0bdcb942ce1fc38fdb22654ac93241363061
Akshay Kamble <kamblea635@gmail.com>
2023-10-03 00:44:08 +0530

    add: notification model

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const cron = require("node-cron");
 const app = express();
 const { sendBasicEmail } = require("./services/email-service");
 const prepareAndStartServer = () => {
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
+
     sendBasicEmail(
       "support@noti.com",
-      "samantdhruv@gmail.com",
+      "kamblea635@gmail.com",
       "This is testing email",
       "This is testing email body"
     );
